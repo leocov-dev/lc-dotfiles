016a8afb71b31eb6181af36e6672e9c60bff6240
Leo Covarrubias <leo@leocov.com>
2020-04-26 14:22:01 -0400

    aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+alias reload!='exec "$SHELL" -l'
+
+alias ..='cd ..'
+
 alias ls='ls --color --classify'
 alias ll="ls -l --human-readable --almost-all"
-alias reload!='exec "$SHELL" -l'
+
+alias df="df -Tha --total"
+alias du="du -ach | sort -h"
+alias free="free -mt"
+
 alias cl="clear"
 alias zshconfig="subl ~/.zshrc"
 alias gas="git add -u && git status -s"
