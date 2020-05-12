c6d8bd3b8b2faca711638db99a25d5b643db935a
Leo Covarrubias <leo.covarrubias@gmail.com>
2020-05-12 14:41:17 -0400

    allow update skip

--- antibody/config.zsh
+++ antibody/config.zsh
@@ -3,5 +3,5 @@
 # setup solarized ls colors
 if [[ ! -f $HOME/.zsh-dircolors.config ]]; then
     log_info 'Initialized plugin: zsh-dircolors'
-    setupsolarized dircolors.ansi-dark
+    setupsolarized dircolors.ansi-dark  # TODO this does not work on MACOS, no `dircolors`
 fi
