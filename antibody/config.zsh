5b7d5d1697d575f298f427fe40d7c2d564f1dcf1
Leo Covarrubias <leo.covarrubias@gmail.com>
2021-03-16 09:12:23 -0400

    rm base16 colors

--- antibody/config.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# setup solarized ls colors
-if [[ ! -f $HOME/.zsh-dircolors.config ]]; then
-    log_info 'Initialized plugin: zsh-dircolors'
-    setupsolarized dircolors.ansi-dark  # TODO this does not work on MACOS, no `dircolors`
-fi
