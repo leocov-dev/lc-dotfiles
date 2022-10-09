4fe5987ac9fead4f7587cea1cb0aa8132bbbe064
Leonardo Covarrubias <leo@Leonardos-Air.skyline>
2022-10-08 20:44:16 -0400

    remove script files

--- zsh/prompt.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-# To customize prompt, run `p10k configure` or edit ~/p10k.zsh.
-# shellcheck disable=SC1090
-[[ ! -f $DOTFILES/zsh/p10k.zsh ]] || source $DOTFILES/zsh/p10k.zsh
-
-export REPORTTIME=10
